717d6211a6c30d2afab8d6f90a045434c6bc3768
abhishekhojha <abhishekhojha45@gmail.com>
2025-05-10 05:01:22 +0530

    refactor: improve formatting and enhance history retrieval response with decrypted URLs

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,15 +10,17 @@ exports.getMe = async (req, res) => {
 };
 exports.getHistory = async (req, res) => {
   try {
-    const userId = req.user.id; 
+    const userId = req.user.id;
     const { page = 1, limit = 10, type } = req.query;
-    
+
     const user = await User.findById(userId);
     console.log(user);
     if (!user || user.deviceID != null) {
-      return res.status(404).json({ success: false, message: "User or device ID not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User or device ID not found" });
     }
-    
+
     const query = {
       userId,
       deviceID: user.deviceID,
@@ -26,18 +28,27 @@ exports.getHistory = async (req, res) => {
     const total = await History.countDocuments(query);
 
     const histories = await History.find(query)
-      .sort({ createdAt: -1 }) 
+      .sort({ createdAt: -1 })
       .skip((page - 1) * limit)
       .limit(parseInt(limit));
+    const decryptedHistories = histories.map((item) => {
+      const decryptedUrl = item.type === "url" ? item.decryptUrl() : undefined;
 
+      return {
+        ...item.toObject(),
+        decryptedUrl,
+      };
+    });
     res.status(200).json({
       success: true,
       totalPages: Math.ceil(total / limit),
       currentPage: parseInt(page),
       totalItems: total,
-      data: histories,
+      data: decryptedHistories,
     });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Server Error", error: err.message });
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: err.message });
   }
 };
